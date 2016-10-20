d007216f09dd1d96ed9675d799ed2a7c065f2a6f
Javier Novo <j.novo@candidohermida.com>
2016-10-20 10:40:55 +0200

    Swagger support through Swashbuckle

--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -8,23 +8,22 @@ namespace WebApp
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
+        public static void Main(string[] args) =>
+            new WebHostBuilder()            
                 .UseKestrel()
                 .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
-        }
+                .Build()
+                .Run();
     }
 	
 	public class Startup
 	{
+        IHostingEnvironment _environment;
         IConfiguration _configuration;
 
         public Startup(IHostingEnvironment env)
         {
+            _environment = env;
             _configuration = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true)
@@ -35,13 +34,21 @@ namespace WebApp
 
 		public void ConfigureServices(IServiceCollection services)
 		{
-			// Add framework services
+            // Add framework services
+            services.AddMvc()
+                .AddJsonOptions(options =>
+                {                    
+                    if (_environment.IsDevelopment())
+                        // SwaggerGen consumes MvcJsonOptions.SerializerSettings
+                        options.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+                });
+            services.AddLogging();
 
-			services.AddMvc();
-			services.AddLogging();
+            // Inject an implementation of ISwaggerProvider with defaulted settings applied
+            services.AddSwaggerGen();
 
-            services.AddOptions();           
-            services.Configure<EventStoreFacade.EventStoreOptions>(_configuration.GetSection(nameof(EventStoreFacade.EventStoreOptions))); // Make IOptions<EventStoreFacade.EventStoreOptions> available through DI
+            services.AddOptions()
+                .Configure<EventStoreFacade.EventStoreOptions>(_configuration.GetSection(nameof(EventStoreFacade.EventStoreOptions))); // Make IOptions<EventStoreFacade.EventStoreOptions> available through DI
 
             services.ConfigureServices();
         }
@@ -56,6 +63,12 @@ namespace WebApp
             loggerFactory.AddConsole();
 
             app.UseMvcWithDefaultRoute();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui assets (HTML, JS, CSS etc.)
+            app.UseSwaggerUi();
 
             app.ConfigureModels(repo, view);
 		}
